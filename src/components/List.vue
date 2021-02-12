049c20dc84012cd5285ea6bc3f8e1b88afae3a7b
RD <hello@prodev.lt>
2021-02-12 19:18:40 +0200

    fuzzy search

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <div>
-            <input type="search" placeholder="Start typing..." class="p-1 text-sm border border-gray-200 w-full">
+            <VueFuse
+                :map-results="true"
+                :list="items"
+                class="p-1 text-sm border border-gray-200 w-full"
+                placeholder="Start typing..."
+                @fuse-results="filterItems"
+                :defaultAll="true" />
         </div>
         <div class="border border-gray-200 mt-4 min-h-200px p-1">
             <ul class="list flex flex-col space-y-1">
@@ -9,7 +15,7 @@
                     class="list-item text-left bg-gray-200 hover:bg-gray-300 p-1 text-sm cursor-pointer"
                     :class="{'bg-gray-300':selected_items.includes(item)}"
                     @click="toggleItem(item)"
-                    v-for="(item,index) in items"
+                    v-for="(item,index) in results"
                     :key="`index_${index}`">{{ item }}</li>
             </ul>
         </div>
@@ -18,6 +24,7 @@
 
 <script>
 import { EventBus } from '../event-bus.js';
+import VueFuse from 'vue-fuse'
 
 export default {
     name: 'list',
@@ -27,13 +34,21 @@ export default {
     data() {
         return {
             selected_items: [],
+            results: [],
         }
+    },
+
+    components: {
+        VueFuse,
     },
 
     watch: {
         selected_items(items) {
             this.$emit('items-changed', items);
-        }
+        },
+        items(items) {
+            this.results = items;
+        },
     },
 
     mounted() {
@@ -52,6 +67,11 @@ export default {
                 if (itemIndex !== -1) {
                     this.selected_items.splice(itemIndex, 1);
                 }
+            }
+        },
+        filterItems(results) {
+            if(results.length) {
+                this.results = results;
             }
         },
     },
